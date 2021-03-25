73f087e0fa9705dc79f487d3255a8f74eb4c753a
Artyom V. Peredery <artperedery@gmail.com>
2021-03-25 18:52:09 +0300

    feat: step #3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/genDiff.js';
 
 const program = new Command();
 
@@ -8,6 +9,7 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((path1, path2) => console.log(genDiff(path1, path2)));
 
 program.parse();
---
+++ src/genDiff.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import path from 'path';
+import fs from 'fs';
+
+export default (path1, path2) => {
+  const file1 = fs.readFileSync(path.resolve(process.cwd(), path1));
+  const file2 = fs.readFileSync(path.resolve(process.cwd(), path2));
+  const firstJson = JSON.parse(file1);
+  const secondJson = JSON.parse(file2);
+  const allKeys = _.uniq([...Object.keys(firstJson), ...Object.keys(secondJson)]);
+  const result = allKeys.reduce((acc, el) => {
+    if (!_.has(secondJson, el)) {
+      return [...acc, `   - ${el} : ${firstJson[el]}`];
+    }
+    if (_.has(secondJson, el)) {
+      if (secondJson[el] !== firstJson[el]) {
+        return [
+          ...acc,
+          `   - ${el} : ${firstJson[el] ?? false}`,
+          `   + ${el} : ${secondJson[el]}`,
+        ];
+      }
+    }
+    return [
+      ...acc,
+      `   ${el} : ${firstJson[el] ?? false}`,
+    ];
+  }, []);
+  return `{\n${result.join('\n')}\n}`;
+};
