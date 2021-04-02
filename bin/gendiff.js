e9f004896367c5f299d1dead2bb1cd1b4b948f53
Artyom V. Peredery <artperedery@gmail.com>
2021-04-03 01:51:27 +0300

    refactor: refactor

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
-  .action((path1, path2) => console.log(genDiff(path1, path2)));
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((path1, path2) => console.log(genDiff(path1, path2, program.opts().format)));
 
-program.parse();
+program.parse(process.argv);
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,34 +1,68 @@
+/* eslint-disable no-trailing-spaces,no-multiple-empty-lines */
 import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
 import getData from './parse.js';
 
-export default (path1, path2) => {
+const buildAst = (before, after) => {
+  const allKeys = _.intersection([...Object.keys(before), ...Object.keys(after)]).sort();
+  console.log(allKeys);
+  return allKeys.reduce(
+    (acc, el) => {
+      let status = '';
+      const key = el;
+      const valueBefore = before[key];
+      const valueAfter = after[key];
+      let value = '';
+      const children = [];
+      if (_.isObject(valueBefore) && _.isObject(valueAfter)) {
+        value = 'nested';
+      }
+      if (!_.has(valueAfter, [key])) {
+        console.log(key);
+        status = 'removed';
+      }
+
+      return [
+        ...acc,
+        {
+          key,
+          value,
+          status,
+          children,
+        },
+      ];
+    }, [],
+  );
+};
+
+export default (path1, path2, format = 'stylish') => {
   const firstFile = fs.readFileSync(path.resolve(process.cwd(), path1));
   const secondFile = fs.readFileSync(path.resolve(process.cwd(), path2));
   const firstParsed = getData(firstFile, path.extname(path1));
   const secondParsed = getData(secondFile, path.extname(path2));
-  const allKeys = _.uniq([...Object.keys(firstParsed), ...Object.keys(secondParsed)]).sort();
-  const result = allKeys.reduce((acc, el) => {
-    if (!_.has(secondParsed, el)) {
-      return [...acc, `  - ${el}: ${firstParsed[el]}`];
-    }
-    if (!_.has(firstParsed, el)) {
-      return [...acc, `  + ${el}: ${secondParsed[el]}`];
-    }
-    if (_.has(secondParsed, el)) {
-      if (secondParsed[el] !== firstParsed[el]) {
-        return [
-          ...acc,
-          `  - ${el}: ${firstParsed[el] ?? false}`,
-          `  + ${el}: ${secondParsed[el]}`,
-        ];
-      }
-    }
-    return [
-      ...acc,
-      `    ${el}: ${firstParsed[el]}`,
-    ];
-  }, []);
-  return `{\n${result.join('\n')}\n}`;
+  const ast = buildAst(firstParsed, secondParsed);
+  console.log(JSON.stringify(ast, null, 2));
+  // const result = allKeys.reduce((acc, el) => {
+  //   if (!_.has(secondParsed, el)) {
+  //     return [...acc, `  - ${el}: ${firstParsed[el]}`];
+  //   }
+  //   if (!_.has(firstParsed, el)) {
+  //     return [...acc, `  + ${el}: ${secondParsed[el]}`];
+  //   }
+  //   if (_.has(secondParsed, el)) {
+  //     if (secondParsed[el] !== firstParsed[el]) {
+  //       return [
+  //         ...acc,
+  //         `  - ${el}: ${firstParsed[el] ?? false}`,
+  //         `  + ${el}: ${secondParsed[el]}`,
+  //       ];
+  //     }
+  //   }
+  //   return [
+  //     ...acc,
+  //     `    ${el}: ${firstParsed[el]}`,
+  //   ];
+  // }, []);
+  // return `{\n${result.join('\n')}\n}`;
 };
