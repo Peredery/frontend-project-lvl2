3d1e112295c4d3e9b57b02bfcd086927c924027c
Artyom V. Peredery <artperedery@gmail.com>
2021-05-29 23:53:04 +0300

    test(json format): fix wrong fixture

--- src/buildAst.js
+++ src/buildAst.js
@@ -4,36 +4,43 @@ const buildAst = (before, after) => {
   const allKeys = _.union([...Object.keys(before), ...Object.keys(after)]).sort();
   return allKeys.map(
     (key) => {
-      let status = 'unchanged';
       const beforeValue = before[key];
       const afterValue = after[key];
-      let value = beforeValue;
+      if (!_.has(before, key)) {
+        return {
+          key,
+          status: 'added',
+          beforeValue,
+          value: afterValue,
+        };
+      }
+      if (!_.has(after, key)) {
+        return {
+          key,
+          status: 'removed',
+          beforeValue,
+          value: afterValue,
+        };
+      }
       if (_.isObject(beforeValue) && _.isObject(afterValue)) {
-        status = 'nested';
-        const children = buildAst(before[key], after[key]);
-        return { key, status, children };
+        return {
+          key,
+          status: 'nested',
+          children: buildAst(beforeValue, afterValue),
+        };
       }
-
-      if (beforeValue !== afterValue) {
-        status = 'changed';
-        value = afterValue;
+      if (!_.isEqual(beforeValue, afterValue)) {
+        return {
+          key,
+          status: 'changed',
+          beforeValue,
+          value: afterValue,
+        };
       }
-
-      if (!_.has(after, [key])) {
-        value = beforeValue;
-        status = 'removed';
-      }
-
-      if (!_.has(before, [key])) {
-        status = 'added';
-        value = afterValue;
-      }
-
       return {
         key,
-        beforeValue,
-        value,
-        status,
+        status: 'unchanged',
+        value: afterValue,
       };
     },
   );
