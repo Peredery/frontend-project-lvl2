45cb2115db1b193333a801d2a320a7b3a629bf19
Artyom V. Peredery <artperedery@gmail.com>
2021-05-30 00:14:15 +0300

    style: move renderMap to Iter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,22 +8,17 @@ const stringify = (value, depth) => {
   return `{\n${rows}\n${space(depth + 1)}}`;
 };
 
-const renderMap = {
-  added: (node, depth) => (`${space(depth)}  + ${node.key}: ${stringify(node.value, depth)}`),
-  removed: (node, depth) => (`${space(depth)}  - ${node.key}: ${stringify(node.beforeValue, depth)}`),
-  changed: (node, depth) => (`${renderMap.removed(node, depth)}\n${renderMap.added(node, depth)}`),
-  unchanged: (node, depth) => (`${space(depth)}    ${node.key}: ${stringify(node.value, depth)}`),
-  // nested: (node, depth) => `${space(depth)}    ${node.key}: ${iter(node.children, depth + 1)}`,
-};
+const iter = (iterAst, iterDepth) => {
+  const renderMap = {
+    added: (node, depth) => (`${space(depth)}  + ${node.key}: ${stringify(node.value, depth)}`),
+    removed: (node, depth) => (`${space(depth)}  - ${node.key}: ${stringify(node.beforeValue, depth)}`),
+    changed: (node, depth) => (`${renderMap.removed(node, depth)}\n${renderMap.added(node, depth)}`),
+    unchanged: (node, depth) => (`${space(depth)}    ${node.key}: ${stringify(node.value, depth)}`),
+    nested: (node, depth) => `${space(depth)}    ${node.key}: ${iter(node.children, depth + 1)}`,
+  };
 
-const iter = (iterAst, depth) => {
-  const result = iterAst.map((node) => {
-    if (node.status === 'nested') {
-      return `${space(depth)}    ${node.key}: ${iter(node.children, depth + 1)}`;
-    }
-    return renderMap[node.status](node, depth);
-  });
-  return ['{', ...result, `${space(depth)}}`].join('\n');
+  const result = iterAst.map((node) => renderMap[node.status](node, iterDepth));
+  return ['{', ...result, `${space(iterDepth)}}`].join('\n');
 };
 
 const render = (ast) => iter(ast, 0);
