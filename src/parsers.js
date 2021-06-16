2cc5363dbb7717c0f47c59cc801a28ce87a1e99a
Artyom V. Peredery <artperedery@gmail.com>
2021-06-16 21:00:56 +0300

    refactor: mentor comment

--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,12 @@
 import yaml from 'js-yaml';
 
-const Parsers = {
+const parsers = {
   json: JSON.parse,
   yml: yaml.load,
   yaml: yaml.load,
 };
 
 export default (file, fileFormat) => {
-  const parse = Parsers[fileFormat];
+  const parse = parsers[fileFormat];
   return parse(file);
 };
